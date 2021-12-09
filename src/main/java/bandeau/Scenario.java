f058a54bbedf65e61d6ab0edc2ae7bf26fc5736e
smajid <smajid@cst-tp-b10615>
2021-12-09 09:59:36 +0100

    finshed

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -39,10 +39,7 @@ public class Scenario {
      * @param b le bandeau ou s'afficher.
      */
     public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
+        MyThread myThread= new MyThread(b, this.myElements);
+        myThread.start();
             }
         }
-    }
-}
